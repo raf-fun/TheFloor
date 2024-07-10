9a5db7ed130e8292826bab75a3c9b08ecf1390a7
rafaelcarterbusiness@gmail.com <rafaelcarterbusiness@gmail.com>
2024-07-09 23:42:59 -0400

    - Added full display for tile - Added backtile

--- TheFloor/TheFloor/wwwroot/site.js
+++ TheFloor/TheFloor/wwwroot/site.js
@@ -4,3 +4,13 @@
         element.classList.toggle('animate');
     }
 };
+function toggleFullScreenAnimation(elementId, isActive) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        if (isActive) {
+            element.classList.add('fullscreen');
+        } else {
+            element.classList.remove('fullscreen');
+        }
+    }
+}
