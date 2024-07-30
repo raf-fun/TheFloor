66443caf306de9752fdcb155108fc5aa3c6a7a44
rafaelcarterbusiness@gmail.com <rafaelcarterbusiness@gmail.com>
2024-07-30 01:25:21 -0400

    - Added font awesome - Added timers for both challenger and challengee - Made start and disable for game - Added random picture switching and disabling button

---
+++ TheFloor/TheFloor/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace TheFloor.Model
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public List<FileData> Images { get; set; } = new List<FileData>();
+        public string BgColor { get; set; }
+    }
+}
