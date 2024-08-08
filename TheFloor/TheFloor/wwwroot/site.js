66d36a384ee06546fde21da03ee5b249aa9b05c8
rafaelcarterbusiness@gmail.com <rafaelcarterbusiness@gmail.com>
2024-08-08 00:20:24 -0400

    - Removed unneeded code

--- TheFloor/TheFloor/wwwroot/site.js
+++ TheFloor/TheFloor/wwwroot/site.js
@@ -48,15 +48,6 @@ function GetFolderFiles(dotNetObject, chunkSize) {
     input.click();
 }
 
-function saveCategories(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-}
-
-function loadCategories (key) {
-    const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : null;
-}
-
 
 var Imagewindow = null;
 function openImageWindow() {
