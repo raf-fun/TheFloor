759f6ca1badf9c800b79076e5cba574ac7e6179a
rafaelcarterbusiness@gmail.com <rafaelcarterbusiness@gmail.com>
2025-04-02 22:42:38 -0400

    - Added variable to show and hide the game screen - Added transition logic to category - Added css for transition

--- TheFloor/TheFloor/Model/Category.cs
+++ TheFloor/TheFloor/Model/Category.cs
@@ -5,5 +5,8 @@
         public string? Name { get; set; }
         public List<FileData> Images { get; set; } = new List<FileData>();
         public string? BgColor { get; set; }
+
+        public bool IsTransitioning { get; set; } = false;
+        public string? TransitionColor { get; set; }
     }
 }
